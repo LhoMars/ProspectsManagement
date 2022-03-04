661ace2c0e9de58d25ac0e3b52a1252b94169eb4
Marcel Lhote <lhote.marcel@gmail.com>
2022-03-04 17:47:15 +0100

    update connection

--- app/src/main/java/fr/prospectsmanagement/LoginActivity.java
+++ app/src/main/java/fr/prospectsmanagement/LoginActivity.java
@@ -1,5 +1,7 @@
 package fr.prospectsmanagement;
 
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +9,10 @@ import fr.prospectsmanagement.dataBase.DaoSQL;
 
 public class LoginActivity extends AppCompatActivity {
     private DaoSQL database;
+    public EditText editIdentifiant = null;
+    public EditText editPassword = null;
+    public Button btnLogin = null;
+    public Button btnForgetPassword = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,17 @@ public class LoginActivity extends AppCompatActivity {
         database = new DaoSQL(this);
         Employee e = new Employee("user","password");
         database.getEmployeeBdd().addemployeeBdd(e);
+
+        setContentView(R.layout.activity_main);
+
+        /* Selection des vues de l'activité */
+        editIdentifiant = (EditText) findViewById(R.id.Identifiant);
+        editPassword = (EditText) findViewById(R.id.MotDePasse);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnForgetPassword = (Button) findViewById(R.id.OublieMotDePasse);
+
+        /*Ecouteur sur le bouton OK*/
+        //btnLogin.setOnClickListener(EcouteurBouton);
 
         setContentView(R.layout.activity_login);
     }
--- app/src/main/java/fr/prospectsmanagement/MainActivity.java
+++ app/src/main/java/fr/prospectsmanagement/MainActivity.java
@@ -7,30 +7,10 @@ import android.os.Bundle;
 import fr.prospectsmanagement.dataBase.DaoSQL;
 
 public class MainActivity extends AppCompatActivity {
-    private DaoSQL database;
-    public EditText editIdentifiant = null;
-    public EditText editPassword = null;
-    public Button btnLogin = null;
-    public Button btnForgetPassword = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         database = new DaoSQL(this);
-
-        Entreprise e = new Entreprise("uneNom", 0);
-        Prospect p = new Prospect("nom", "prenom", "tel", "mail", 1, e);
-        database.getProspectBdd().addProspectBdd(p);
-
-        setContentView(R.layout.activity_main);
-
-        /* Selection des vues de l'activité */
-        editIdentifiant = (EditText) findViewById(R.id.Identifiant);
-        editPassword = (EditText) findViewById(R.id.MotDePasse);
-        btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnForgetPassword = (Button) findViewById(R.id.OublieMotDePasse);
-
-        /*Ecouteur sur le bouton OK*/
-        //btnLogin.setOnClickListener(EcouteurBouton);
     }
 }
