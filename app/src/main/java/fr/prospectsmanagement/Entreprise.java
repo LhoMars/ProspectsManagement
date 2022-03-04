42690b0ab7cbd7e30f18fc49e41b95143b35166d
Marcel Lhote <lhote.marcel@gmail.com>
2022-03-04 16:11:15 +0100

    ajout des bdd

--- app/src/main/java/fr/prospectsmanagement/Entreprise.java
+++ app/src/main/java/fr/prospectsmanagement/Entreprise.java
@@ -3,4 +3,9 @@ package fr.prospectsmanagement;
 public class Entreprise {
     private String nom;
     private int siret;
+
+    public Entreprise(String nom, int siret) {
+        this.nom = nom;
+        this.siret = siret;
+    }
 }
--- app/src/main/java/fr/prospectsmanagement/Prospect.java
+++ app/src/main/java/fr/prospectsmanagement/Prospect.java
@@ -8,4 +8,36 @@ public class Prospect {
     private int notes;
     private Entreprise lEntreprise;
 
+    public Prospect(String nom, String prenom, String tel, String mail, int notes, Entreprise lEntreprise) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.tel = tel;
+        this.mail = mail;
+        this.notes = notes;
+        this.lEntreprise = lEntreprise;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public int getNotes() {
+        return notes;
+    }
+
+    public Entreprise getlEntreprise() {
+        return lEntreprise;
+    }
 }
