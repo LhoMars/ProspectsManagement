af61b415d319d6710ba1c1a5aaa878fb6a1babc3
KennBey <eliotout.lilian.al@gmail.com>
2022-03-07 13:59:11 +0100

    Passage de Main en Menu

---
+++ app/src/main/java/fr/prospectsmanagement/MenuActivity.java
@@ -0,0 +1,21 @@
+package fr.prospectsmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import fr.prospectsmanagement.dataBase.DaoSQL;
+
+public class MenuActivity extends AppCompatActivity {
+    private DaoSQL dataBase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dataBase = new DaoSQL(this);
+
+        Entreprise e = new Entreprise("uneNom", 0);
+        Prospect p = new Prospect("nom", "prenom", "tel", "mail", 1, e);
+        dataBase.getProspectBdd().addProspectBdd(p);
+
+        setContentView(R.layout.activity_menu);
+    }
+}
