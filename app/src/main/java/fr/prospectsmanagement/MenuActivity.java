c8f3905a1061ad60bb8dfdfdb9005357e0b7879e
KennBey <eliotout.lilian.al@gmail.com>
2022-03-21 14:00:43 +0100

    Nouvelle page ajout

--- app/src/main/java/fr/prospectsmanagement/MenuActivity.java
+++ app/src/main/java/fr/prospectsmanagement/MenuActivity.java
@@ -3,17 +3,22 @@ package fr.prospectsmanagement;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.drawable.AnimationDrawable;
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.graphics.drawable.AnimationDrawable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import fr.prospectsmanagement.api.ApiBdd;
 import fr.prospectsmanagement.dataBase.DaoSQL;
 
 public class MenuActivity extends AppCompatActivity {
     private DaoSQL dataBase;
+    public Button btnAjouter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,19 @@ public class MenuActivity extends AppCompatActivity {
 
 
 
+        /* Sélection du bouton par son ID puis on ajoute son événement */
+        btnAjouter = (Button) findViewById(R.id.btnLogin);
+        btnAjouter.setOnClickListener(eventBtnAjouter);
+    }
+        public View.OnClickListener eventBtnAjouter = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent AjoutProspect = new Intent(MenuActivity.this, AjoutProspectActivity.class);
+                startActivity(AjoutProspect);
+            }
+        };
+
+
         /* Animation du Background mise en coommentaire pour l'instant, couleurs à changer.*/
 
         /*RelativeLayout RelativeLayout = findViewById(R.id.menuLayout);
@@ -35,7 +53,6 @@ public class MenuActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(2500);
         animationDrawable.setExitFadeDuration(5000);
         animationDrawable.start();*/
-    }
 
     private void boiteMessage(String msg) {
         AlertDialog.Builder boite = new AlertDialog.Builder(MenuActivity.this);
